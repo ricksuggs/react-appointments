49135ee3e0e487710054c8da4e651ea9a4e94587
Rick Suggs <suggsra@gmail.com>
2019-02-16 21:25:49 -0600

    Updates

---
+++ precache-manifest.158c51f91245e533dadd77f93d189e82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "422aba8639312ac889d34200226a5c4e",
+    "url": "/react-appointments/static/media/icons-16.422aba86.woff"
+  },
+  {
+    "revision": "a3d422bbf4ff5a1d95bd",
+    "url": "/react-appointments/static/css/main.209bbc7d.chunk.css"
+  },
+  {
+    "revision": "da8d71e3d79b78e0f578",
+    "url": "/react-appointments/static/js/runtime~main.da8d71e3.js"
+  },
+  {
+    "revision": "5502b03b7b07c388f20f77fa469a1f47",
+    "url": "/react-appointments/static/media/icons-20.5502b03b.ttf"
+  },
+  {
+    "revision": "0920e71261dea952a1ae",
+    "url": "/react-appointments/static/js/2.0920e712.chunk.js"
+  },
+  {
+    "revision": "7f037192fcdef96884010177e90e8c5f",
+    "url": "/react-appointments/static/media/icons-16.7f037192.eot"
+  },
+  {
+    "revision": "a3d422bbf4ff5a1d95bd",
+    "url": "/react-appointments/static/js/main.a3d422bb.chunk.js"
+  },
+  {
+    "revision": "f1bd87971a332ecb21b4ac1d19537fa3",
+    "url": "/react-appointments/static/media/icons-20.f1bd8797.eot"
+  },
+  {
+    "revision": "a82b41082b04f774d82317dff6874d14",
+    "url": "/react-appointments/static/media/icons-16.a82b4108.ttf"
+  },
+  {
+    "revision": "52f2f35bf3a635ca696e95cc33cf12fa",
+    "url": "/react-appointments/static/media/icons-20.52f2f35b.woff"
+  },
+  {
+    "revision": "0920e71261dea952a1ae",
+    "url": "/react-appointments/static/css/2.b63e1952.chunk.css"
+  },
+  {
+    "revision": "093d3f061cde3f4f9de40554b03312b5",
+    "url": "/react-appointments/index.html"
+  }
+];
--- src/actions/index.js
+++
@@ -1,3 +0,0 @@
-import { createAction } from 'redux-starter-kit';
-
-export const updateTimeSlot = createAction('UPDATE_TIME_SLOT');
